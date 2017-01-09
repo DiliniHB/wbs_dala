4621b7a46996d09c70f0467d30a434867513e067
DiliniHB <dilinihb@gmail.com>
2017-01-09 12:30:33 +0530

    configured sectors

--- education/damage_losses/urls.py
+++ education/damage_losses/urls.py
@@ -5,4 +5,5 @@ from .import views
 urlpatterns = [
 
     url(r'^dl_govn_edu_facilities$', views.dl_govn_edu_facilities, name='dl_govn_edu_facilities'),
+    #url(r'^dl_govn_edu_facilities$', views.dl_govn_edu_facilities, name='dl_govn_edu_facilities'),
 ]
